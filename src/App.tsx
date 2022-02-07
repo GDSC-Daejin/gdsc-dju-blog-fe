8dcbceae0f343bf1bfc4d77a332f505d8295792b
HYEOK_E <jhjeong00@gmail.com>
2022-02-07 15:57:24 +0900

    feat: darkmode toggle button add

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import './App.css';
-import { theme } from './styles/theme';
+import { darkTheme, lightTheme } from './styles/theme';
 import Layout from './Layout';
+import { useTheme } from './hooks/useTheme';
+import { GlobalStyle } from './styles/globalStyle';
 
 function App() {
+  const [themeMode, setTheme] = useTheme();
+
+  const theme = themeMode === 'light' ? lightTheme : darkTheme;
+  console.log(themeMode);
   return (
     <ThemeProvider theme={theme}>
+      <button onClick={setTheme as () => void}>toggle</button>
+      <GlobalStyle />
       <Layout />
     </ThemeProvider>
   );
