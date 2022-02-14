fa30a67ecbcbf5d044c110967ff5d22a44c8adb4
HYEOK_E <jhjeong00@gmail.com>
2022-02-14 13:16:23 +0900

    chore: test button delete

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import { ContainerInner, LayoutContainer } from '../../styles/layouts';
-import { useTheme } from '../../hooks/useTheme';
 import BlogCard from '../../components/common/BlogCard';
 import { BlogCardWrapper, CardSection } from './styled';
-import { useRecoilState } from 'recoil';
-import { loaderState } from '../../store/loader';
 
 const Home = () => {
   const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const [loader, setLoader] = useRecoilState(loaderState);
   return (
     <>
       <LayoutContainer>
@@ -20,13 +16,6 @@ const Home = () => {
               </BlogCardWrapper>
             ))}
           </CardSection>
-          <button
-            onClick={() => {
-              setLoader({ ...loader, loading: true, background: true });
-            }}
-          >
-            asdasdssa
-          </button>
         </ContainerInner>
       </LayoutContainer>
     </>
