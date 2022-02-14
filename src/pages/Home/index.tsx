3809312f2bdea0d46ebfe0c40db33eddf34201d8
HYEOK_E <jhjeong00@gmail.com>
2022-02-14 13:13:49 +0900

    feat: loader Recoil setting, background props add

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,9 +3,12 @@ import { ContainerInner, LayoutContainer } from '../../styles/layouts';
 import { useTheme } from '../../hooks/useTheme';
 import BlogCard from '../../components/common/BlogCard';
 import { BlogCardWrapper, CardSection } from './styled';
+import { useRecoilState } from 'recoil';
+import { loaderState } from '../../store/loader';
 
 const Home = () => {
   const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const [loader, setLoader] = useRecoilState(loaderState);
   return (
     <>
       <LayoutContainer>
@@ -17,6 +20,13 @@ const Home = () => {
               </BlogCardWrapper>
             ))}
           </CardSection>
+          <button
+            onClick={() => {
+              setLoader({ ...loader, loading: true, background: true });
+            }}
+          >
+            asdasdssa
+          </button>
         </ContainerInner>
       </LayoutContainer>
     </>
