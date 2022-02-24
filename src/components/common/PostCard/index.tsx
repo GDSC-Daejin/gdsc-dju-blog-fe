6d905ecf10185a54da8203497c8ca0fbded8f62f
HYEOK_E <jhjeong00@gmail.com>
2022-02-25 00:53:27 +0900

    feat: query string add

--- src/components/common/PostCard/index.tsx
+++ src/components/common/PostCard/index.tsx
@@ -36,12 +36,12 @@ const PostCard = (props: Iprops) => {
   };
   const dateFilter = () => {
     const dateArray = date.slice(0, 10).split('-');
-    return `${dateArray[0]}.${dateArray[1]}.${dateArray[2]}`;
+    return `${dateArray[0].slice(2, 4)}.${dateArray[1]}.${dateArray[2]}`;
   };
   return (
     <PostCardWrapper
       onMouseOver={() => setHover(true)}
-      onMouseOut={() => setHover(false)}
+      onMouseLeave={() => setHover(false)}
     >
       <PostCardImageWrapper>
         <PostCardImage src={MockPostImage} />
