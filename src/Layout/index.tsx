ce44bec7f326ef1a1a9c25a988e220266f302bb4
dongddddd <habaik0147@gmail.com>
2022-03-05 17:42:08 +0900

    Revert "feat: navigation bar add"
    This reverts commit 4bf18181eb7c2c08510ffdc43d67e5a45309a692.

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,14 +1,12 @@
 import React, { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import GoogleSpinner from '../components/common/GoogleSpinner';
-import Navigation from '../components/common/Navigation';
 
 const Home = lazy(() => import('../pages/Home'));
 
 const Layout = () => {
   return (
     <>
-      <Navigation />
       <Suspense fallback={<GoogleSpinner />}>
         <Routes>
           <Route path={'/*'} element={<Home />} />
