ccb2275c289fe690684f53a74b79a942ec46de3c
byung ju choi <bjc30849@gmail.com>
2022-03-17 19:25:19 +0900

    chore: react-hook-form 테스트 코드 추가

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { LayoutContainer } from '../../styles/layouts';
+import { SignUpForm, SignUpInputBox } from './styled';
 
 const SignUp = () => {
   const {
@@ -7,7 +9,32 @@ const SignUp = () => {
     register,
     formState: { errors },
   } = useForm();
-  return <div>SIGNUP</div>;
+  const onSubmit = (values: any) => console.log(values);
+  return (
+    <LayoutContainer>
+      <SignUpForm onSubmit={handleSubmit(onSubmit)}>
+        <SignUpInputBox
+          type="email"
+          placeholder="Email"
+          {...register('email', {
+            required: 'Required',
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: 'invalid email address',
+            },
+          })}
+        />
+        {errors.email && errors.email.message}
+        <SignUpInputBox
+          {...register('username', {
+            validate: (value) => value !== 'admin' || 'Nice try!',
+          })}
+        />
+        {errors.username && errors.username.message}
+        <button type="submit">가입하기</button>
+      </SignUpForm>
+    </LayoutContainer>
+  );
 };
 
 export default SignUp;
--- src/pages/SignUp/styled.tsx
+++ src/pages/SignUp/styled.tsx
@@ -1,1 +1,13 @@
 import styled from 'styled-components';
+
+export const SignUpForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SignUpInputBox = styled.input`
+  width: 100px;
+  height: 50px;
+`;
