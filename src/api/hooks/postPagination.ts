f257f6b8cfee119f94541ec8431d154789baa703
HYEOK_E <jhjeong00@gmail.com>
2022-03-27 20:43:43 +0900

    chore: myblog post pagination size modify

--- src/api/hooks/postPagination.ts
+++ src/api/hooks/postPagination.ts
@@ -1,7 +1,7 @@
 export const url = (category: string, page: number) => {
   if (category === 'all') {
-    return `?page=${page}`;
+    return `?page=${page}&size=6`;
   } else {
-    return `?category=${category}&page=${page}`;
+    return `?category=${category}&page=${page}&size=6`;
   }
 };
