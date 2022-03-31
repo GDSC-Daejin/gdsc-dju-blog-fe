552bb7e07e00dcc0d16ba13388baf75ebe4924ac
dongddddd <habaik0147@gmail.com>
2022-03-31 23:51:49 +0900

    post fix

--- src/pages/Posts/index.tsx
+++ src/pages/Posts/index.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import { Giscus } from '@giscus/react';
-import { useRecoilState } from 'recoil';
-import { themeState } from '../../store/theme';
 import { ContainerInner, LayoutContainer } from '../../styles/layouts';
-
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor } from '@toast-ui/react-editor';
 const Posts = () => {
-  const [themeMode] = useRecoilState(themeState);
+  const editorRef: any = createRef<HTMLInputElement>();
+
+  const handleClick = () => {
+    editorRef.current.getInstance().exec('Bold');
+  };
   return (
-    <LayoutContainer>
-      <ContainerInner>
-        <Giscus
-          repo="GDSC-Daejin/gdsc-dju-blog-fe"
-          repoId="R_kgDOGwlX0Q"
-          category="Announcements"
-          categoryId="DIC_kwDOGwlX0c4CBQA5"
-          mapping="pathname"
-          theme={themeMode.light ? 'light' : 'dark'}
-          lang="ko"
-        />
-      </ContainerInner>
-    </LayoutContainer>
+    <>
+      <LayoutContainer>
+        <ContainerInner>
+          <Giscus
+            repo="GDSC-Daejin/gdsc-dju-blog-fe"
+            repoId="R_kgDOGwlX0Q"
+            category="Announcements"
+            categoryId="DIC_kwDOGwlX0c4CBQA5"
+            mapping="pathname"
+            lang="ko"
+          />
+        </ContainerInner>
+      </LayoutContainer>
+      <Editor
+        previewStyle="vertical"
+        height="400px"
+        initialEditType="markdown"
+        initialValue="hello"
+        ref={editorRef}
+      />
+    </>
   );
 };
 
