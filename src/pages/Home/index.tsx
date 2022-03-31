10810ab2e3e905c7c5ac71ad0aa8fa3757fd07ac
byung ju choi <bjc30849@gmail.com>
2022-03-31 11:20:15 +0900

    chore: 로그인 테스트코드 수정

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,12 +5,28 @@ import BlogCard from '../../components/common/BlogCard';
 import { CardSection, BlogCardWrapper } from './styled';
 
 const Home = () => {
+  const data = {
+    id: 'gudcks0305',
+    password: '$10$8lDyClwH.ET3BA44inQLKuRNISg4paTPwgD2V5pw/RMmtTGJvhPvy',
+  };
+  const handleClick = () => {
+    fetch('https://gdsc-dju.com/test/auth/login', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((response) => console.log('Success:', JSON.stringify(response)));
+  };
   return (
     <>
       <LayoutContainer>
         <CardSection>
           <BlogCard />
         </CardSection>
+        <button onClick={handleClick}>로그인</button>;
       </LayoutContainer>
     </>
   );
