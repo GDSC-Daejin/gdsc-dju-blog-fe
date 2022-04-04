eeaff7ce324d9ddff3f022215cac3bfe94d4e2a6
dongddddd <habaik0147@gmail.com>
2022-04-04 22:06:31 +0900

    chore: style fix

--- src/components/common/Animation/index.tsx
+++ src/components/common/Animation/index.tsx
@@ -4,14 +4,12 @@ export const SideBarAnimation = {
   isActive: {
     x: 0,
     transition: {
-      delay: 0.1,
       duration: 0.2,
     },
   },
   isUnActive: {
     x: -486,
     transition: {
-      delay: 0.1,
       duration: 0.2,
     },
   },
@@ -21,14 +19,12 @@ export const SideBarCategoryAnimation = {
     translateX: 10,
     color: '#191F28',
     transition: {
-      delay: 0.1,
       duration: 0.3,
     },
     borderBottom: '1px solid #191F28',
   },
   isUnActive: {
     transition: {
-      delay: 0.1,
       duration: 0.3,
     },
   },
@@ -40,32 +36,6 @@ export const SideBarCircleAnimation = {
   },
   isUnActive: {
     x: -20,
-    opacity: 0,
-  },
-};
-export const hoverMotion = {
-  isActive: {
-    translateY: -10,
-    color: '#191F28',
-    transition: {
-      delay: 0.1,
-      duration: 0.3,
-    },
-    borderBottom: '1px solid #000',
-  },
-  isUnActive: {
-    translateY: 0,
-    color: '#D1D6DB',
-    borderBottom: '1px solid #fff',
-  },
-};
-export const circleMotion = {
-  isActive: {
-    opacity: 1,
-    y: 0,
-  },
-  isUnActive: {
-    y: -20,
     opacity: 0,
   },
 };
