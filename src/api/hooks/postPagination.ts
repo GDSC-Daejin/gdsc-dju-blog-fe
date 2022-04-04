15e8a83e08046b328fc087cae54c7105e054144d
HYEOK_E <jhjeong00@gmail.com>
2022-04-04 21:30:46 +0900

    chore: post common 요청 uri 수정

--- src/api/hooks/postPagination.ts
+++ src/api/hooks/postPagination.ts
@@ -1,6 +1,9 @@
 export const url = (category: string, page: number) => {
   if (category === 'all') {
     return `?page=${page}&size=6`;
+  }
+  if (category === 'common') {
+    return `/common/?size=6&featured=true`;
   } else {
     return `/${category}?page=${page}&size=6`;
   }
