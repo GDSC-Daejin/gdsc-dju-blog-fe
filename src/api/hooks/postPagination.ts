f6129f14185284099ccadad8e3db35bc40b980c5
HYEOK_E <jhjeong00@gmail.com>
2022-04-04 22:44:38 +0900

    chore: modify category

--- src/api/hooks/postPagination.ts
+++ src/api/hooks/postPagination.ts
@@ -1,9 +1,6 @@
 export const url = (category: string, page: number) => {
   if (category === 'all') {
     return `?page=${page}&size=6`;
-  }
-  if (category === 'common') {
-    return `/common/?size=6&featured=true`;
   } else {
     return `/${category}?page=${page}&size=6`;
   }
