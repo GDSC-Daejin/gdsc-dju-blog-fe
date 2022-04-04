893a3ce8587c5a61da21656f98e23ed85cecf71d
HYEOK_E <jhjeong00@gmail.com>
2022-04-04 18:27:51 +0900

    chore: fix errorboundary

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,13 @@ import './App.css';
 import Layout from './Layout';
 import { theme } from './styles/theme';
 import GoogleLoader from './components/common/GoogleLoader';
-import { AnimatePresence } from 'framer-motion';
 import { ErrorBoundary } from 'react-error-boundary';
-import Error from './pages/Error';
+import ErrorFallback from './pages/ErrorFallback';
 
 function App() {
   return (
     <>
-      <ErrorBoundary FallbackComponent={Error}>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
         <Suspense fallback={<GoogleLoader background={false} />}>
           <ThemeProvider theme={theme}>
             <Layout />
