7959dc79ffe2b6730d0c5e34443614f8731d545b
HYEOK_E <jhjeong00@gmail.com>
2022-04-14 16:23:46 +0900

    chore: modify button

--- src/components/common/Button/styled.ts
+++ src/components/common/Button/styled.ts
@@ -11,27 +11,27 @@ export const StyledButton = styled.button<{
   display: flex;
   align-items: center;
   border: 1px solid ${(props) => props.theme.color.grey300};
-  ${(props) =>
-    props.border &&
-    css`
-      border: 1px solid ${props.border};
-    `}
+  font-size: ${(props) => props.theme.fontSize.body2};
   padding: 8px 30px;
   cursor: pointer;
-  font-size: ${(props) => props.theme.fontSize.body2};
-  ${(props) =>
-    props.disable &&
+  box-shadow: 0 2px 12px rgba(25, 31, 40, 0.08);
+  border-radius: 50px;
+  ${({ border }) =>
+    border &&
+    css`
+      border: 1px solid ${border};
+    `}
+  ${({ disable }) =>
+    disable &&
     css`
       cursor: not-allowed;
       opacity: 0.5;
     `}
-  ${(props) =>
-    props.size === 'large' &&
+  ${({ size }) =>
+    size === 'large' &&
     css`
       padding: 8px 86px;
     `}
-  box-shadow: 0 2px 12px rgba(25, 31, 40, 0.08);
-  border-radius: 50px;
   ${({ background }) =>
     background &&
     css`
