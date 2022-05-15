b78a3291bec324e7c7817c2655e12e85bd3b20d4
HYEOK_E <jhjeong00@gmail.com>
2022-05-15 21:18:30 +0900

    chore: modify index.tsx for react18

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import { SWRConfig } from 'swr';
 
-const container = document.getElementById('app');
+const container = document.getElementById('root');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 
 root.render(
