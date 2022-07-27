539b0e85f3ac60571d1751e3fc9102647a1cffb6
byung ju choi <bjc30849@gmail.com>
2022-07-28 08:22:35 +0900

    chore: props 수정

--- src/components/common/BlogCardGridLayout/index.tsx
+++ src/components/common/BlogCardGridLayout/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BlogCardGridLayoutStyle, BlogCardWrapper } from './styled';
 import BlogCard from '../BlogCard/index';
 import { DetailPostDataType } from '../../../types/postData';
+import { useGetMyScrapData } from '../../../api/hooks/useGetMyScrapData';
 
 interface IBlogCardGridLayout {
   PostData: DetailPostDataType[];
@@ -9,12 +10,17 @@ interface IBlogCardGridLayout {
 
 const BlogCardGridLayout: React.FC<IBlogCardGridLayout> = (props) => {
   const { PostData } = props;
+  const { scrapData } = useGetMyScrapData();
+  const scrapList =
+    scrapData?.data.content.map((v) => {
+      return v.post[0].postId;
+    }) ?? [];
 
   return (
     <BlogCardGridLayoutStyle>
       {PostData.map((data) => (
         <BlogCardWrapper key={data.postId}>
-          <BlogCard postData={data} />
+          <BlogCard postData={data} isScrap={scrapList.includes(data.postId)} />
         </BlogCardWrapper>
       ))}
     </BlogCardGridLayoutStyle>
